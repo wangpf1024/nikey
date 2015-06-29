60ee9afc98cfb7b0e85663cef5eb9a400b071e54
ittarvin <882977wpf>
2015-06-29 17:46:07 +0800

    策略模式

---
+++ src/main/java/net/docs/design/pattern/strategy/ICalculator.java
@@ -0,0 +1,10 @@
+package net.docs.design.pattern.strategy;
+
+/**
+ * ²ßÂÔÄ£Ê½
+ * 
+ * @author arvin
+ */
+public interface ICalculator {
+	 public int calculate(String exp);  
+}
---
+++ src/main/java/net/docs/design/pattern/strategy/StrategyTest.java
@@ -0,0 +1,12 @@
+package net.docs.design.pattern.strategy;
+
+public class StrategyTest {
+
+	public static void main(String[] args) {
+		String exp = "2+8";  
+        ICalculator cal = new Plus();  
+        int result = cal.calculate(exp);  
+        System.out.println(result);  
+	}
+
+}
