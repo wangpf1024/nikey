69d41b05b896e9a7ef84728d1cb4664bd8036101
ittarvin <882977wpf>
2015-06-29 17:02:04 +0800

    原型模式

---
+++ src/main/java/net/docs/design/pattern/prototype/SerializableObject.java
@@ -0,0 +1,8 @@
+package net.docs.design.pattern.prototype;
+
+import java.io.Serializable;
+
+public class SerializableObject implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+}
