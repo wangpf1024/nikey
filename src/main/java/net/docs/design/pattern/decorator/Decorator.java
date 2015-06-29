2156a854f1e9b9f0524e3ca57a68ce06ed6c6e91
ittarvin <882977wpf>
2015-06-29 17:29:33 +0800

    装饰模式

---
+++ src/main/java/net/docs/design/pattern/decorator/Decorator.java
@@ -0,0 +1,25 @@
+package net.docs.design.pattern.decorator;
+
+/**
+ * в╟йндёй╫
+ * 
+ * @author arvin
+ */
+public class Decorator implements Sourceable {
+
+	private Sourceable source;
+
+	public Decorator(Sourceable source) {
+		super();
+		this.source = source;
+	}
+
+	@Override
+	public void method() {
+		// TODO Auto-generated method stub
+		System.out.println("before decorator!");
+		source.method();
+		System.out.println("after decorator!");
+	}
+
+}
---
+++ src/main/java/net/docs/design/pattern/decorator/Sourceable.java
@@ -0,0 +1,5 @@
+package net.docs.design.pattern.decorator;
+
+public interface Sourceable {
+	 public void method();  
+}
