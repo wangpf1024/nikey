6552e6288d0b9c9693ea7178378168f89f57813b
ittarvin <882977wpf>
2015-06-29 17:39:41 +0800

    组合模式

---
+++ src/main/java/net/docs/design/pattern/composite/TreeNode.java
@@ -0,0 +1,55 @@
+package net.docs.design.pattern.composite;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+/**
+ * 组合模式
+ *
+ * @author arvin
+ */
+public class TreeNode {
+
+	private String name;
+	private TreeNode parent;
+	private Vector<TreeNode> children = new Vector<TreeNode>();
+
+	public TreeNode(String name) {
+		this.name = name;
+	}
+
+
+	  //添加孩子节点
+    public void add(TreeNode node){
+        children.add(node);
+    }
+
+    //删除孩子节点
+    public void remove(TreeNode node){
+        children.remove(node);
+    }
+
+    //取得孩子节点
+    public Enumeration<TreeNode> getChildren(){  
+        return children.elements();  
+    }  
+    
+    
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public TreeNode getParent() {
+		return parent;
+	}
+
+	public void setParent(TreeNode parent) {
+		this.parent = parent;
+	}
+	
+	
+}
