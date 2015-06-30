21cb97ba16cd28471e77733f3ed04a66490528d3
ittarvin <882977wpf>
2015-06-30 18:58:15 +0800

    使用redis 添加用户的注册和登录功能-需要优化代码

--- src/main/java/net/nikey/controllers/homeController.java
+++ src/main/java/net/nikey/controllers/homeController.java
@@ -3,11 +3,19 @@ package net.nikey.controllers;
 
 
 
+import net.nikey.bean.RetwisSecurity;
+import net.nikey.interceptor.CookieInterceptor;
+import net.nikey.redis.RetwisRepository;
 import net.paoding.rose.web.Invocation;
+import net.paoding.rose.web.annotation.Param;
 import net.paoding.rose.web.annotation.Path;
 import net.paoding.rose.web.annotation.rest.Get;
 import net.paoding.rose.web.annotation.rest.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,6 +30,13 @@ import java.util.Map;
 @Path("")
 public class homeController {
 
+    @Autowired
+    private final RetwisRepository retwis;
+
+    @Autowired
+    public homeController(RetwisRepository twitter) {
+        this.retwis = twitter;
+    }
 
     @Get("")
     @Post("")
@@ -30,5 +45,60 @@ public class homeController {
         v.addModel("slogan", "Just do what you feel like");
         return "home";
     };
+
+
+
+    @Get("/signIn")
+    public String signIn(Invocation v){
+        return "signin";
+    };
+
+    @Post("/signIn")
+    public String signIn(@Param("name") String name, @Param("pass") String pass,HttpServletResponse response) {
+        // add tracing cookie
+        if (retwis.auth(name, pass)) {
+            addAuthCookie(retwis.addAuth(name), name, response);
+            return "@ successful";
+        }
+        else if (StringUtils.hasText(name) || StringUtils.hasText(pass)) {
+           return "@ failed";
+        }
+        // go back to sign in screen
+        return "r:/signIn";
+    }
+
+
+    private void addAuthCookie(String auth, String name, HttpServletResponse response) {
+        RetwisSecurity.setUser(name, retwis.findUid(name));
+
+        Cookie cookie = new Cookie(CookieInterceptor.RETWIS_COOKIE, auth);
+        cookie.setComment("Retwis-J demo");
+        // cookie valid for up to 1 week
+        cookie.setMaxAge(60 * 60 * 24 * 7);
+        response.addCookie(cookie);
+    }
+
+
+    @Get("/signUp")
+    public String signUp(Invocation v){
+        return "signup";
+    };
+
+    @Post("/signUp")
+    public String signUp( @Param("name")String name, @Param("pass") String pass,@Param("pass2")String pass2,HttpServletResponse response) {
+
+        if (retwis.isUserValid(name)) {
+            return "@ user is already existed";
+        }
+
+        if (!StringUtils.hasText(pass) || !StringUtils.hasText(pass2) || !pass.equals(pass2)) {
+            return "@ these passwords are not equal";
+        }
+
+        String auth = retwis.addUser(name, pass);
+        addAuthCookie(auth, name, response);
+
+        return "r:/signIn";
+    }
 
 }
