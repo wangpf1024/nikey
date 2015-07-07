b2b177dd2eb9101026583b1509c7cb10d2a32884
ittarvin <882977wpf>
2015-07-07 16:10:21 +0800

    单元页面添加登录限制

--- src/main/java/net/nikey/controllers/module/IndexController.java
+++ src/main/java/net/nikey/controllers/module/IndexController.java
@@ -1,6 +1,7 @@
 package net.nikey.controllers.module;
 
 import net.nikey.annotations.LoginRequired;
+import net.nikey.utils.NikeySecurity;
 import net.paoding.rose.web.Invocation;
 import net.paoding.rose.web.annotation.Param;
 import net.paoding.rose.web.annotation.Path;
@@ -19,6 +20,9 @@ public class IndexController {
     //首页
     @Get("/{uid:[0-9]+}")
     public String get(Invocation v,@Param("uid") int uid){
+        if(!NikeySecurity.isSignedIn()){
+            return "r:/e1";
+        };
         Map<String,Object> map = new HashMap<String, Object>();
         v.addModel("uid",uid);
         return "module";
